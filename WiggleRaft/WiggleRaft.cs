112a8e7fc0094da271f487abc6f17508c221c3b0
ssinakhot <steven.sinakhot@gmail.com>
2020-11-16 16:27:27 -0500

    initial commit

---
+++ WiggleRaft/WiggleRaft.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WiggleRaft : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod WiggleRaft has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod WiggleRaft has been unloaded!");
+    }
+}
